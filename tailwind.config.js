71544c3389adde43ff301fb46bea0ef76bd103c1
Kallolx <kallol.business.ds@gmail.com>
2025-01-24 16:25:13 +0600

    Update site with new responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: 'class',
   theme: {
     extend: {
@@ -19,6 +24,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    aspectRatio
   ],
 };
